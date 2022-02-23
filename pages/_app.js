d4e2c49a1b806c17e57557fdbc3120c29602d29e
nunofontes <nunof.h.fontes@gmail.com>
2022-02-23 23:55:26 +0000

    added files for login and other updates

--- pages/_app.js
+++ pages/_app.js
@@ -7,15 +7,21 @@ import '../styles/globals.css'
 import AddUser from '../components/Users/AddUser';
 
 
-export default function App() {
+export default function App({ Component, pageProps }) {
   const router = useRouter();
   const [user, setUser] = useState(null);
   const [authenticated, setAuthenticated] = useState(false);
 
   useEffect(() => {
 
+    console.log("Doing auth check");
+
     // on initial load - do an auth check 
     authCheck(router.asPath);
+
+    // on route change start - hide page content by setting authorized to false  
+    const hideContent = () => setAuthorized(false);
+    router.events.on('routeChangeStart', hideContent);
     
     // on route change start - hide page content by setting authenticated to false     
     router.events.on('routeChangeStart', authCheck);
@@ -27,6 +33,7 @@ export default function App() {
     // from the event with the `off` method:
     return () => {
       // router.events.off('routeChangeStart', handleRouteChange)
+      router.events.off('routeChangeStart', hideContent);
       router.events.off('routeChangeComplete', authCheck);
     }
 
@@ -45,20 +52,22 @@ export default function App() {
     
     if(!userService.userValue && !publicPaths.includes(urlPath)) {
       setAuthenticated(false);
-        router.push({
-            pathname: '/login',
-            query: { returnUrl: router.asPath }
-        });
+      console.log("setting Auth to false");
+      router.push({
+          pathname: '/login',
+          query: { returnUrl: router.asPath }
+      });
     } else {
-      setAuthenticated(false);
+      console.log("setting Auth to true");
+      setAuthenticated(true);
     }
 
   }
 
   // <AddUser />
   return (
-    <div className="bg-black h-screen font-sans flex items-center w-full bg-teal-lighter justify-center">
-
+    <div className=" h-screen font-sans flex items-center w-full bg-teal-lighter justify-center">
+      <title>Next.js 11 - User Registration and Login Example</title>
       {authenticated &&
         <Component {...pageProps} />
       }
